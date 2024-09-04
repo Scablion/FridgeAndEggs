2c5cbbfa190ca9ec987eb9ae01d673a8cfcfe6bb
Камиль Халилуллин <133690959+Scablion@users.noreply.github.com>
2024-09-04 22:00:20 +0300

    Update Main.java

--- Progect1/src/Main.java
+++ Progect1/src/Main.java
@@ -1,12 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int eggs = 2;
+        int eggs = 3;
         if (eggs < 3){
             System.out.print("Рекомендую позавтракать бутербродами. ");
         }
-        else if (eggs > 3) {
+        else{
             System.out.print("Рекомендую приготовить омлет. ");
         }
         System.out.println("Приятного аппетита!");
