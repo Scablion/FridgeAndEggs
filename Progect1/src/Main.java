204c0de857294a7c0eb7f2fed6554f85d2c33b0b
Камиль Халилуллин <133690959+Scablion@users.noreply.github.com>
2024-09-04 21:36:48 +0300

    Add files via upload

---
+++ Progect1/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int eggs = 2;
+        if (eggs < 3){
+            System.out.print("Рекомендую позавтракать бутербродами. ");
+        }
+        else if (eggs > 3) {
+            System.out.print("Рекомендую приготовить омлет. ");
+        }
+        System.out.println("Приятного аппетита!");
+    }
+}
